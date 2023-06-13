570494c8ad6da64f357a05b204b3beb96c4ff5cc
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-06-13 13:18:45 +0600

    selected classes data loaded

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6pylfvj.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -37,10 +37,28 @@ async function run() {
 
     // selected Classes Collection
 
+    app.get("/selectedClasses", async (req, res) => {
+      const email = req.query.email;
+
+      if (!email) {
+        res.send([]);
+      }
+      const query = { email: email };
+      const result = await selectedClassesCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/selectedClasses", async (req, res) => {
       const selectedClasses = req.body;
       console.log(selectedClasses);
       const result = await selectedClassesCollection.insertOne(selectedClasses);
+      res.send(result);
+    });
+
+    app.delete("/selectedClasses/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectedClassesCollection.deleteOne(query);
       res.send(result);
     });
 
