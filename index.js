1bece2cd5ffd25fe3782015fc6a071e0d45255af
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-06-14 22:27:38 +0600

    admin role created

--- index.js
+++ index.js
@@ -78,6 +78,18 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      res.send(result);
+    });
+
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -116,7 +128,7 @@ async function run() {
       if (email !== decodedEmail) {
         return res
           .status(403)
-          .send({ error: true, message: "unauthorized access" });
+          .send({ error: true, message: "forbidden access" });
       }
 
       const query = { email: email };
